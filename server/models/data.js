7e562010418029083a6eccb1d682571fc76fc65c
rifaturrana <smrifaturrana@gmail.com>
2023-03-28 23:21:02 +0600

    First

---
+++ server/models/data.js
@@ -0,0 +1,60 @@
+const {Schema,model}=require('mongoose');
+
+const dataSchema=new Schema({
+
+    end_year:{
+        type:String,
+    },
+    intensity:{
+        type:Number,
+    },
+    sector:{
+        type:String,
+    },
+    topic:{
+        type:String,
+    },
+    insight:{
+        type:String,
+    },
+    url:{
+        type:String,
+    },
+    region:{
+        type:String,
+    },
+    start_year:{
+        type:String,
+    },
+    impact:{
+        type:String,
+    },
+    added:{
+        type:String,
+    },
+    published:{
+        type:String,
+    },
+    country:{
+        type:String,
+    },
+    relevance:{
+        type:Number,
+    },
+    pestle:{
+        type:String,
+    },
+    source:{
+        type:String,
+    },
+    title:{
+        type:String,
+    },
+    likelihood:{
+        type:Number,
+    }
+
+});
+
+
+module.exports=model('data',dataSchema);
---
+++ server/server.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const bodyparser = require("body-parser");
+// const sample=require('./jsondata.json')
+// const data=require('./models/data')
+const path = require("path");
+const app = express();
+app.use(cors({ origin: "*" }));
+app.use(express.json());
+app.use(bodyparser.json());
+
+app.use(bodyparser.urlencoded({ extended: true }));
+
+dotenv.config();
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    })
+    .then(() => console.log("DB Connected"))
+    .catch((err) => {
+    console.log("DB Connection Error: " + err.message);
+    });
+
+
+const port = process.env.PORT || 3001;
+
+// const seedDatas = async () => {
+//     try {
+//       await data.deleteMany();
+//       console.log("data are deleted");
+//       await data.insertMany(sample);
+//       console.log("data products are added.");
+//       process.exit();
+//     } catch (error) {
+//       console.log(error.message);
+//       process.exit();
+//     }
+//   };
+  
+//   seedDatas();
+
+app.listen(port, () => {
+  console.log("listening on port " + port);
+});
